4ddd0bd34f0fb07d8334619599ae07785fe780fc
Hyejin Kim <idgafff010@gmail.com>
2025-03-06 01:48:30 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/kotlin/com/hyejin/account_book/AccountBookApplication.kt
@@ -0,0 +1,11 @@
+package com.hyejin.account_book
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccountBookApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccountBookApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyejin/account_book/AccountBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyejin.account_book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
