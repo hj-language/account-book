61c30162056019ee2da3971922f65dedf1c08c85
Hyejin Kim <idgafff010@gmail.com>
2025-03-16 22:01:00 +0900

    [미션4] 조회 REST API 만들기
    feat: 컨트롤러 개발 - type 기반 카테고리 목록 조회 API 추가

--- src/main/kotlin/com/hyejin/account_book/domain/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/hyejin/account_book/domain/presentation/controller/PresentationApiController.kt
@@ -1,14 +1,19 @@
 package com.hyejin.account_book.domain.presentation.controller
 
+import com.hyejin.account_book.domain.presentation.dto.CategoryDTO
+import com.hyejin.account_book.domain.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
-    @GetMapping("/test")
-    fun test(): String {
-        return "OK"
+class PresentationApiController(
+    private val presentationService: PresentationService,
+) {
+    @GetMapping("/v1/categories")
+    fun getCategoriesByType(@RequestParam categoryType: String): List<CategoryDTO> {
+        return presentationService.getCategoriesByType(categoryType)
     }
 }
