4718f2596127327eca7f71c60281c0328b965505
Hyejin Kim <idgafff010@gmail.com>
2025-03-16 21:33:10 +0900

    refactor: 불필요 생성자 제거

--- src/main/kotlin/com/hyejin/account_book/domain/presentation/dto/TransactionRecordDTO.kt
+++ src/main/kotlin/com/hyejin/account_book/domain/presentation/dto/TransactionRecordDTO.kt
@@ -1,19 +1,11 @@
 package com.hyejin.account_book.domain.presentation.dto
 
 import com.hyejin.account_book.domain.entity.Category
-import com.hyejin.account_book.domain.entity.TransactionRecord
 import java.time.LocalDate
 
 data class TransactionRecordDTO(
-        val amount: Int,
-        val memo: String,
-        val transactionDate: LocalDate,
-        val category: Category
-) {
-    constructor(transactionRecord: TransactionRecord) : this(
-            amount = transactionRecord.amount,
-            memo = transactionRecord.memo,
-            transactionDate = transactionRecord.transactionDate,
-            category = transactionRecord.category
-    )
-}
+    val amount: Int,
+    val memo: String,
+    val transactionDate: LocalDate,
+    val category: Category
+)
