48c12e5a2209e35b6033b62e76669c782aada8f9
Hyejin Kim <idgafff010@gmail.com>
2025-03-16 21:36:28 +0900

    feat: 서비스 개발 - type 기반 카테고리 조회하여 DTO 리턴

---
+++ src/main/kotlin/com/hyejin/account_book/domain/presentation/service/PresentationService.kt
@@ -0,0 +1,24 @@
+package com.hyejin.account_book.domain.presentation.service
+
+import com.hyejin.account_book.domain.presentation.dto.CategoryDTO
+import com.hyejin.account_book.domain.presentation.repository.PresentationRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class PresentationService(
+    private val presentationRepository: PresentationRepository,
+) {
+    @Transactional(readOnly = true)
+    fun getCategoriesByType(type: String): List<CategoryDTO> {
+        val categories = presentationRepository.getCategoriesByType(type)
+
+        return categories.map {
+            CategoryDTO(
+                id = it.id!!,
+                name = it.name,
+                type = it.type
+            )
+        }
+    }
+}
