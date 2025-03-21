62a6059e27d2e9e025828063079dc448f63320eb
Hyejin Kim <idgafff010@gmail.com>
2025-03-21 20:08:34 +0900

    feat: TransationRecordRepository에 구현한 거래 내역 조회 메서드 호출하는 로직 구현
    - 카테고리가 있으면 카테고리 조건 추가
    - 카테고리가 없으면 전체 카테고리 대상으로 조회

--- src/main/kotlin/com/hyejin/account_book/domain/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/hyejin/account_book/domain/presentation/repository/PresentationRepository.kt
@@ -1,14 +1,30 @@
 package com.hyejin.account_book.domain.presentation.repository
 
 import com.hyejin.account_book.domain.entity.Category
+import com.hyejin.account_book.domain.entity.TransactionRecord
 import com.hyejin.account_book.domain.repository.CategoryRepository
+import com.hyejin.account_book.domain.repository.TransactionRecordRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Repository
 class PresentationRepository(
     private val categoryRepository: CategoryRepository,
+    private val transactionRecordRepository: TransactionRecordRepository,
 ) {
     fun getCategoriesByType(type: String): List<Category> {
         return categoryRepository.findByType(type)
+    }
+
+    fun getTransactions(userId: Long, start: LocalDate, end: LocalDate, categoryId: Long?): List<TransactionRecord> {
+        if (categoryId == null) {
+            return transactionRecordRepository.findByTransactionDateBetweenAndUserId(
+                start, end, userId
+            )
+        }
+        
+        return transactionRecordRepository.findByTransactionDateBetweenAndCategoryIdAndUserId(
+            start, end, categoryId, userId
+        )
     }
 }
