ccb72db8846311fdf0a77252450ec22affb614ab
Hyejin Kim <idgafff010@gmail.com>
2025-03-21 19:59:38 +0900

    fix: 거래 DTO에서 카테고리도 카테고리 DTO로 타입 변경

--- src/main/kotlin/com/hyejin/account_book/domain/presentation/dto/TransactionRecordDTO.kt
+++ src/main/kotlin/com/hyejin/account_book/domain/presentation/dto/TransactionRecordDTO.kt
@@ -1,11 +1,10 @@
 package com.hyejin.account_book.domain.presentation.dto
 
-import com.hyejin.account_book.domain.entity.Category
 import java.time.LocalDate
 
 data class TransactionRecordDTO(
     val amount: Int,
     val memo: String,
     val transactionDate: LocalDate,
-    val category: Category
+    val category: CategoryDTO
 )
